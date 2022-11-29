3c3a45f8f1959e424f9b29dbe8c9a08867ad68fe
Vlerie <Valerie.Basaly@mail.ru>
2022-11-29 16:37:36 +0300

    Метод печати массива

--- Program.cs
+++ Program.cs
@@ -18,3 +18,13 @@ void SecondArray(string[] array1, string[] array2)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
